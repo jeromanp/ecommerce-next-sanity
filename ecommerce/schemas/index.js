a06420888d493149af52434bcc0fd9e1288f00d4
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-10-29 12:26:05 -0600

    add: schemas sanity

--- ecommerce/schemas/index.js
+++ ecommerce/schemas/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import product from './product';
+import banner from './banner';
+
+
+export const schemaTypes = [
+  product,
+  banner,
+  // Otros esquemas si los tienes
+];
