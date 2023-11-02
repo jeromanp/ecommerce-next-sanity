47d5b9e1a657ed983f5ee9803673bf249337302a
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-01 23:52:42 -0600

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import { Cart } from "./";
+// import { useStateContext } from "../context/StateContext";
 
 export default function Navbar() {
+  // const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">RoPi Shoes</Link>
+      </p>
+
+      <button
+        type="button"
+        className="cart-icon"
+        // onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        {/* <span className="cart-item-qty">{totalQuantities}</span> */}
+        <span className="cart-item-qty">1</span>
+      </button>
+
+      {/* {showCart && <Cart />} */}
+    </div>
+  );
 }
