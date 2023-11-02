9e29c76106bc19df4d66d336b7914da242401277
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-02 15:11:01 -0600

    style

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -57,11 +57,11 @@ export default function ProductDetail({ product, products }) {
             </div>
             <p>(20)</p>
           </div>
-          <h4>Details: </h4>
+          <h4>Detalles: </h4>
           <p>{details}</p>
           <p className="price">${price}</p>
           <div className="quantity">
-            <h3>Quantity:</h3>
+            <h3>Cantidad:</h3>
             <p className="quantity-desc">
               <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
@@ -79,17 +79,17 @@ export default function ProductDetail({ product, products }) {
               className="add-to-cart"
               onClick={() => onAdd(product, qty)}
             >
-              Add to Cart
+              Agregar al carrito
             </button>
             <button type="button" className="buy-now" onClick={handleBuyNow}>
-              Buy Now
+              Comprar Ahora
             </button>
           </div>
         </div>
       </div>
 
       <div className="maylike-products-wrapper">
-        <h2>You may also like</h2>
+        <h2>También te puede gustar</h2>
         <div className="marquee">
           <div className="maylike-products-container track">
             {products.map((item) => (
