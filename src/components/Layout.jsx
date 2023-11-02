94c925d6465c49fafaf0dc7e2ff796fedccf54ae
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-01 22:30:55 -0600

    create Layuout from children components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
-    <div>Layout</div>
-  )
+    <div className="layout">
+      <Head>
+        <title>RoPi Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
