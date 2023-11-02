6fe9cb7a284b242da6997bea21d2b7d3fc15666e
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-01 23:49:38 -0600

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer-container">
+      <p>2023 RoPi Shoes All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 }
