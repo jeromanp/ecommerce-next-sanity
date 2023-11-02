7481413863d3a53dde3ec415f0e19a83d94f5f27
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-02 17:11:07 -0600

    add cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 
-import { Cart } from "./";
-// import { useStateContext } from "../context/StateContext";
+import { Cart } from "./index";
+import { useStateContext } from "../../context/StateContext";
 
 export default function Navbar() {
-  // const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
     <div className="navbar-container">
@@ -17,14 +17,13 @@ export default function Navbar() {
       <button
         type="button"
         className="cart-icon"
-        // onClick={() => setShowCart(true)}
+        onClick={() => setShowCart(true)}
       >
         <AiOutlineShopping />
-        {/* <span className="cart-item-qty">{totalQuantities}</span> */}
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
 
-      {/* {showCart && <Cart />} */}
+      {showCart && <Cart />}
     </div>
   );
 }
